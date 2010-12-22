73a7d516ca636a3c2cb6a37ee2c627a3f8eefafe
optixx <david@optixx.org>
2010-12-22 13:16:57 +0100

    cleanup

--- Debug/src/cr_startup_lpc11.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc11.d src/cr_startup_lpc11.o: ../src/cr_startup_lpc11.c \
-  C:\Documents\ and\ Settings\Administrator\My\ Documents\workspace\CMSISv1p30_LPC11xx\inc/system_LPC11xx.h
-
-C:\Documents\ and\ Settings\Administrator\My\ Documents\workspace\CMSISv1p30_LPC11xx\inc/system_LPC11xx.h:
--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -47,77 +47,77 @@ C_DEPS += \
 src/0VideoOut.o: ../src/0VideoOut.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O3 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/0VideoOut.d" -o"$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O3 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/0VideoOut.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/3d.o: ../src/3d.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O3 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/3d.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O3 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/3d.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/D3DDemo.o: ../src/D3DDemo.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/D3DDemo.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/D3DDemo.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/PaletteDemo.o: ../src/PaletteDemo.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/PaletteDemo.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/PaletteDemo.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/ParticleDemo.o: ../src/ParticleDemo.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/ParticleDemo.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/ParticleDemo.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/Shell.o: ../src/Shell.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/Shell.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/Shell.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/SpriteDemo.o: ../src/SpriteDemo.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/SpriteDemo.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/SpriteDemo.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/TileDemo.o: ../src/TileDemo.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/TileDemo.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/TileDemo.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/math3D.o: ../src/math3D.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/math3D.d" -o"$@" "$<"
+	arm-none-eabi-gcc -x c++ -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/math3D.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/rbox_main.o: ../src/rbox_main.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"C:\Documents and Settings\Administrator\My Documents\workspace\CMSISv1p30_LPC11xx\inc" -O2 -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/rbox_main.d" -o"$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC11xx -D__CODE_RED -D__REDLIB__ -I"E:\workspace\CMSISv1p30_LPC11xx\inc" -O2 -O2 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/rbox_main.d" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
